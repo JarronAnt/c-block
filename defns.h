1d503fc18bcc8a3645e805d4a2c1de206d844516
SSPYR0 <jarronblizzard@gmail.com>
2018-04-05 18:32:37 -0400

    Restructure
    clean up the code a bit

---
+++ defns.h
@@ -0,0 +1,56 @@
+#include <ctime>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+//Transaction
+struct Transaction{
+    //basic transaction data 
+    double amount;
+    string senderKey;
+    string revieverKey;
+    time_t timestamp; 
+};
+
+//Block
+class Block{
+    private:
+        //index
+        int index;
+        //all the hash values 
+        size_t hash;
+        size_t prevHash;
+        size_t genHash();
+
+    public:
+        //constructor 
+        Block(int index, Transaction transaction, size_t prevHash); 
+
+        //getters 
+        size_t getHash(); 
+        size_t getPrevHash(); 
+
+        //transaction data member 
+        Transaction data; 
+
+        //validate hash
+
+        bool isValid();
+};
+
+//blockchain
+class Blockchain{
+    private:
+        Block createGenBlock();
+
+    public:
+        vector<Block> chain;
+        Blockchain();
+
+        void addBlock(Transaction data);
+        void isChainValid();
+
+        //to test demo only
+        Block *getLatestBlock();
+};  
--- main.cpp
+++ main.cpp
@@ -1,49 +1,8 @@
-#include <ctime>
-#include <vector>
-#include <string>
+#include "defns.h"
 
-using namespace std;
-
-//Transaction
-struct Transaction{
-    //basic transaction data 
-    double amount;
-    string senderKey;
-    string revieverKey;
-    time_t timestamp; 
-};
-
-//Block
-class Block{
-    private:
-        //index
-        int index;
-        //all the hash values 
-        size_t hash;
-        size_t prevHash;
-        size_t genHash;
-
-    public:
-        //constructor 
-        Block(int index, Transaction transaction, size_t prevHash); 
-
-        //getters 
-        size_t getHash(); 
-        size_t getPrevHash(); 
-
-        //transaction data member 
-        Transaction data; 
-
-        //validate hash
-
-        bool isValid();
-};
-
-//blockchain
-class Blockchain{
-    private:
-        Block createGenBlock();
-
-    public:
-        vector<Block> chain;
-};  
+Block::Block(int idx, Transaction d, size_t prevHash){
+    index = idx;
+    data = d;
+    prevHash = prevHash;
+    hash = genHash();
+}
